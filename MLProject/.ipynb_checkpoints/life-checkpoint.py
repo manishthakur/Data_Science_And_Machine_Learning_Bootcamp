35dde712031a00e8be5f39e08d858463b7b7739c
Manish Thakur <manishhakur.in@hotmail.com>
2022-10-03 14:03:42 -0400

    first commit

---
+++ MLProject/.ipynb_checkpoints/life-checkpoint.py
@@ -0,0 +1,12 @@
+import math
+
+theAnswer = 42
+
+def square_root(x):
+    return math.sqrt(x)
+
+def quote_marvin():
+    print('I have calculated your chance of survival, but I don''t think you will like it.')
+    
+def quote_marvin01():
+    print('I have calculated your chance of survival, but I don''t think you will like it.')
